764617c2d01a9b65bd3da485647c685b93bd2555
Youssefassoul <assoulyoussef10@gmail.com>
2025-08-12 10:51:14 +0100

    second update

---
+++ week1/day1/Exercises-xp-gold/exercise3.py
@@ -0,0 +1,7 @@
+while True:
+    user_input = input("Enter your name: ")
+    if user_input == "youssef":
+        print(f"Hello, {user_input}!")
+        break
+    else:
+        continue
--- week1/day1/exercises/exercise5.py
+++ week1/day1/exercises/exercise5.py
@@ -1,24 +1,25 @@
-# Step 1: Create a set with your favorite numbers
 my_fav_numbers = set()
 
-# Step 2: Add two new numbers
 my_fav_numbers.add(73)
 my_fav_numbers.add(98)
 my_fav_numbers.add(99)
 my_fav_numbers.add(2)
 
-# Step 3: Remove one number (simulate "last number")
-my_fav_numbers.pop()
+print(my_fav_numbers)
+
+my_fav_numbers.remove(99)
+print(my_fav_numbers)
 
 
-# Step 4: Create a set with your friend’s favorite numbers
 friend_fav_numbers = set()
-friend_fav_numbers.add(45)  # Adding an additional favorite number
-friend_fav_numbers.add(33)  # Adding an additional favorite number
-friend_fav_numbers.add(5)  # Adding an additional favorite number
+friend_fav_numbers.add(45)
+friend_fav_numbers.add(33)
+friend_fav_numbers.add(5)
 
-# Step 5: Combine both sets
+print(friend_fav_numbers)
+
+
 our_fav_numbers = my_fav_numbers.union(friend_fav_numbers)
 
-# Output the result
+
 print(our_fav_numbers)
