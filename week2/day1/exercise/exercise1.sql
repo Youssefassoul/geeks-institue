af67fc69b6fee47612578feebefb31f27b5c12cf
Youssefassoul <assoulyoussef10@gmail.com>
2025-08-22 10:46:49 +0100

    remove outdated SQL script for items and customers tables

--- week2/day1/exercise/exercise1.sql
+++ week2/day1/exercise/exercise1.sql
@@ -1,50 +1,46 @@
-CREATE DATABASE public;
-
--- Create tables
-CREATE TABLE IF NOT EXISTS items (
-  id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  name TEXT NOT NULL,
-  price INTEGER NOT NULL
+CREATE TABLE items(
+ item_id SERIAL PRIMARY KEY,
+ item_name VARCHAR (300) NOT NULL,
+ price DECIMAL(10,2) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS customers (
-  id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  first_name TEXT NOT NULL,
-  last_name TEXT NOT NULL
+CREATE TABLE customers(
+ actor_id SERIAL PRIMARY KEY,
+ first_name VARCHAR (50) NOT NULL,
+ last_name VARCHAR (100) NOT NULL
 );
 
--- Insert items
-INSERT INTO items (name, price) VALUES
-  ('Small Desk', 100),
-  ('Large desk', 300),
-  ('Fan', 80);
+INSERT INTO items (item_name, price)
+VALUES('Small Desk', 100);
 
--- Insert customers
-INSERT INTO customers (first_name, last_name) VALUES
-  ('Greg', 'Jones'),
-  ('Sandra', 'Jones'),
-  ('Scott', 'Scott'),
-  ('Trevor', 'Green'),
-  ('Melanie', 'Johnson');
+INSERT INTO items (item_name, price)
+VALUES('Large Desk', 300);
 
--- Queries
--- 1) All the items
-SELECT * FROM items;
+INSERT INTO items (item_name, price)
+VALUES('Fan', 80);
 
--- 2) All the items with a price above 80 (80 not included)
+INSERT INTO customers (first_name, last_name)
+VALUES('Greg', 'Jones');
+INSERT INTO customers (first_name, last_name)
+VALUES('Sandra', 'Jones');
+INSERT INTO customers (first_name, last_name)
+VALUES('Scott', 'Scott');
+INSERT INTO customers (first_name, last_name)
+VALUES('Trevor', 'Green');
+INSERT INTO customers (first_name, last_name)
+VALUES('Melanie', 'Johnson');
+
+
+SELECT * FROM customers
+SELECT * FROM itemd
+
 SELECT * FROM items WHERE price > 80;
 
--- 3) All the items with a price below 300 (300 included)
-SELECT * FROM items WHERE price <= 300;
+SELECT * FROM items WHERE price < 300;
 
--- 4) All customers whose last name is 'Smith'
--- Outcome: returns 0 rows with current data
-SELECT * FROM customers WHERE last_name = 'Smith';
+SELECT * FROM customers WHERE last_name = 'Smith'
 
--- 5) All customers whose last name is 'Jones'
-SELECT * FROM customers WHERE last_name = 'Jones';
+SELECT * FROM customers WHERE last_name = 'Jones'
 
--- 6) All customers whose firstname is not 'Scott'
-SELECT * FROM customers WHERE first_name <> 'Scott';
-
+SELECT * FROM customers WHERE NOT first_name = 'Scott'
 
