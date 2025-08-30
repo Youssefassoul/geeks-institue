6eb3db1e5c58f44f3d64d400649df4c36756cbe4
Youssefassoul <assoulyoussef10@gmail.com>
2025-08-30 11:03:11 +0100

    update my deleted folder

--- week1/day1/exercises/exercise3.py
+++
@@ -1,6 +0,0 @@
-name = input("What's your name? ")
-if name == "Alice":
-    print(f"Hello, {name}!")
-else:
-    print("Hello, stranger!")
-    print("Welcome to the program!")
--- week2/day4/exercises/main.py
+++
@@ -1,4 +0,0 @@
-from menu_editor import show_user_menu
-
-if __name__ == "__main__":
-    show_user_menu()
